a394d889c2302f977c3f4e36276aa44c483fe73e
OliverCox <oliver@schmine.com>
2013-03-18 17:58:42 +0000

    changed port for mongodb

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb');
 module.exports = {
   dbconnection: new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1"
-                                   , process.env.OPENSHIFT_MONGODB_DB_PORT || 27018, {}),
+                                   , process.env.OPENSHIFT_MONGODB_DB_PORT || 27017, {}),
   ipaddr: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
   port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
   dbUser: 'admin',
